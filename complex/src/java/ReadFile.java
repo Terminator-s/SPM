7cbb657e0272b590250574d6e8a86de615f50ab4
rusnlk <rushda.najeeb@ifsworld.com>
2019-08-08 00:20:12 +0530

    code base modified

--- complex/src/java/ReadFile.java
+++ complex/src/java/ReadFile.java
@@ -1,19 +1,33 @@
+import controlStructure.ControlStructure;
+
 import java.io.*;
 
 public class UploadFile implements iUploadFile {
+    File uploadFile = null;
+    int noOfLines = 0;
 
     @Override
     public void getFile(File uploadFile) {
         StringBuilder sb = new StringBuilder();
-        try
 
-        {
+        ControlStructure cs = new ControlStructure();
+        try{
+            //read the file
             FileReader reader = new FileReader("C:\\Users\\rusnlk\\Desktop\\EmployeeService.java");
             BufferedReader buff = new BufferedReader(reader);
             String line = null;
+
+            //iterate line by line
             while ((line = buff.readLine()) != null) {
-                sb.append(line);
+
+                //call for size factor
+                cs.getValueForLine();
+                //call for inheritance factor
+
+
+                noOfLines++;
             }
+
             buff.close();
             reader.close();
 
--- complex/src/java/iReadFile.java
+++ complex/src/java/iReadFile.java
@@ -1,7 +1,6 @@
 import java.io.File;
 
 public interface iUploadFile {
-    File uploadFile = null;
 
     void getFile(File uploadFile);
 }
